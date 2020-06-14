c66e2c5b058d698406c108773d364696d0098f04
Bioready <Bioready@gmail.com>
2020-06-14 13:31:34 -0500

    Updating files

--- ClarionInnReport/FindPeakBandwidth.py
+++ ClarionInnReport/FindPeakBandwidth.py
@@ -1,17 +1,32 @@
 import pandas as pd
+import glob
+from datetime import timedelta
 
-data = pd.read_csv('ClarionInnUsage.csv')
-data_index = data.set_index('Date')
-
-print('----- WEEKLY BANDWIDTH USAGE -----')
+# Function to dynamically find CSV
 
 
-Week1 = data.loc[1:92, 'Outbound'].max() / 1000000
-Week2 = data.loc[92:188, 'Outbound'].max() / 1000000
-Week3 = data.loc[189:284, 'Outbound'].max() / 1000000
-Week4 = data.loc[285:374, 'Outbound'].max() / 1000000
+def pd_read_pattern(*Args):
+    files = glob.glob(*Args)
+    data = pd.DataFrame()
+    for f in files:
+        data = data.append(pd.read_csv(f, header=9))
+        data['Outbound'].dropna(how='all')
+    return data.reset_index(drop=True)
 
-WeeklyBandwidth = (Week1, Week2, Week3, Week4)
 
-for weeks in WeeklyBandwidth:
-    print(str(round(weeks, 2)), 'Mbps')
+# Open CSV
+data = pd_read_pattern('*- Traffic -*.csv')
+
+
+# Define spans of weeks
+Week1 = data.iloc[0:85]
+Week2 = data.iloc[85:169]
+Week3 = data.iloc[169:253]
+Week4 = data.iloc[253:337]
+
+Month = [Week1, Week2, Week3, Week4]
+
+print('Week 1 - ', str(round(Week1['Outbound'].max() / 1000000, 2)), 'Mbps')
+print('Week 2 - ', str(round(Week2['Outbound'].max() / 1000000, 2)), 'Mbps')
+print('Week 3 - ', str(round(Week3['Outbound'].max() / 1000000, 2)), 'Mbps')
+print('Week 4 - ', str(round(Week4['Outbound'].max() / 1000000, 2)), 'Mbps')
--- RandomWineSelector/quickstart.py
+++ RandomWineSelector/quickstart.py
@@ -6,14 +6,15 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# The ID of a sample document.
-DOCUMENT_ID = '195j9eDD3ccgjQRttHhJPymLJUCOUjs-jmwTrekvdjFE'
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SAMPLE_RANGE_NAME = 'Class Data!A2:E'
 
 def main():
-    """Shows basic usage of the Docs API.
-    Prints the title of a sample document.
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
     """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
@@ -34,13 +35,21 @@ def main():
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
-    service = build('docs', 'v1', credentials=creds)
+    service = build('sheets', 'v4', credentials=creds)
 
-    # Retrieve the documents contents from the Docs service.
-    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range=SAMPLE_RANGE_NAME).execute()
+    values = result.get('values', [])
 
-    print('The title of the document is: {}'.format(document.get('title')))
-
+    if not values:
+        print('No data found.')
+    else:
+        print('Name, Major:')
+        for row in values:
+            # Print columns A and E, which correspond to indices 0 and 4.
+            print('%s, %s' % (row[0], row[4]))
 
 if __name__ == '__main__':
     main()
